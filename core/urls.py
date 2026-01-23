ba362a189151fbdea0014a8ec933e8227b158a2c
marcosVictorM <marcosvictormelo@gmail.com>
2026-01-23 09:09:50 -0300

    corrigindo import urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import StatusPontoView, RegistrarPontoView, relatorio_mensal, views # <--- Adicione o import aqui
+from .views import StatusPontoView, RegistrarPontoView, relatorio_mensal, gerar_relatorio_pdf # <--- Adicione o import aqui
 
 urlpatterns = [
     path('status/', StatusPontoView.as_view(), name='status-ponto'),
     path('registrar/', RegistrarPontoView.as_view(), name='registrar-ponto'),
     path('historico/', relatorio_mensal, name='historico'), 
-    path('api/relatorio-pdf/', views.gerar_relatorio_pdf, name='relatorio_pdf'),
+    path('api/relatorio-pdf/', gerar_relatorio_pdf, name='relatorio_pdf'),
 ]
